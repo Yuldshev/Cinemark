6fe470a2b51690c61e4a4af77667420b257a8876
Yuldshev <ikrom921@gmail.com>
2025-07-20 19:16:59 +0500

    Add network layer
    Used Alamofire & API endpoints

---
+++ Cinemark/Core/Network/NetworkError.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+enum NetworkError: Error, LocalizedError {
+  case noConnection
+  case serverError(statusCode: Int)
+  case decodingFailed(error: Error)
+  case badRequest(error: Error)
+  case unknown(error: Error)
+  
+  var errorDescription: String? {
+    switch self {
+      case .noConnection: return "No internet connection. Please check your settings."
+      case .serverError(let code): return "Server error with status code: \(code)"
+      case .decodingFailed: return "Failed to process data from the server."
+      case .badRequest: return "Invalid request. Please try again later."
+      case .unknown: return "An unknown error occurred."
+    }
+  }
+}
