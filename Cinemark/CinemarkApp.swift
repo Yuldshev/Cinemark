9628d8001f670226ef441234081ba5fbbe61227b
Yuldshev <ikrom921@gmail.com>
2025-07-20 13:56:35 +0500

    Initial Commit

---
+++ Cinemark/CinemarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemarkApp.swift
+//  Cinemark
+//
+//  Created by T on 20/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CinemarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
